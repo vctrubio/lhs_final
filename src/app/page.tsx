8b0e79d450d66fbee153f0c7596078c9e257f1dd
vctrubio <vctrubio@gmail.com>
2024-11-25 16:00:55 +0100

    # tsconfig alias for root dir

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import { TakeOne } from './playground';
-import { fetchEntriesContentful } from "../../backend/apisConnections";
+import { fetchEntriesContentful } from "#/backend/apisConnections";
+
 export default async function Home() {
   const { properties } = await fetchEntriesContentful();
 
