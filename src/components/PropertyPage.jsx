7e31358b9f66e8f1ecedf6c3a3e353f78241c248
vctrubio <vctrubio@gmail.com>
2024-12-05 11:48:30 +0100

    padding fix on the y

--- src/components/PropertyPage.jsx
+++ src/components/PropertyPage.jsx
@@ -93,7 +93,7 @@ export default function PropiedadPage({ property }) {
     };
 
     return (
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
+        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             {/* Image Gallery Section */}
             <div className="mb-12">
                 {/* Main Image Slider */}
