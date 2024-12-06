1a3ff71e95f5a2200e0c2d49d3ed3f3372b6b885
vctrubio <vctrubio@gmail.com>
2024-12-06 11:01:50 +0100

    images update

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.ctfassets.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '',
+        pathname: '/**',
+      },
+      // Add any other domains you need to support
+      // {
+      //   protocol: 'https',
+      //   hostname: 'example.com',
+      //   port: '',
+      //   pathname: '/**',
+      // },
+    ],
   },
 };
 
