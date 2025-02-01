272595f985687ef010f2b87d5b8b76f934f498f5
vctrubio <vctrubio@gmail.com>
2025-02-01 19:04:44 +0100

    fetch server for pdf page

--- src/app/(propiedades)/[slug]/pdf/page.tsx
+++ src/app/(propiedades)/[slug]/pdf/page.tsx
@@ -1,29 +1,17 @@
-'use client'
-import React, { useEffect, useState } from 'react';
 import { fetchPropertyByID } from '#/backend/apisConnections';
-import { Property } from '#/backend/types';
 import { PropertyBroucher } from '@/components/PropertyPageBrochure';
 import { PdfParent, CreatePdf } from '@/components/PdfPageView';
 
 type PageParams = { slug: string }
-export default function PdfView({ params }: { params: Promise<PageParams>; }) {
-    const [property, setProperty] = useState<Property | null>(null);
+export default async function PdfView({ params }: { params: PageParams; }) {
+    const fetchedProperty = await fetchPropertyByID(params.slug);
 
-    useEffect(() => {
-        async function fetchData() {
-            const resolvedParams = await params;
-            const fetchedProperty = await fetchPropertyByID(resolvedParams.slug);
-            setProperty(fetchedProperty);
-        }
-        fetchData();
-    }, [params]);
-
-    if (!property) {
-        return <div>Property not found</div>;
+    if (!fetchedProperty) {
+        return <div>--page not found exec--</div>;
     }
 
-    const pdf = new PdfParent(property);
-    const brochure = <PropertyBroucher property={property} flag={true} />;
+    const pdf = new PdfParent(fetchedProperty);
+    const brochure = <PropertyBroucher property={fetchedProperty} flag={true} />;
 
     return (
         <CreatePdf pdf={pdf} brochure={brochure} />
