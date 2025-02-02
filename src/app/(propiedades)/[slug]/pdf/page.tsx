66d2462b0628cc914a4034d6fb2ec2c6c280ffe5
vctrubio <vctrubio@gmail.com>
2025-02-02 22:12:23 +0100

    nicky stage4 prod ready

--- src/app/(propiedades)/[slug]/pdf/page.tsx
+++ src/app/(propiedades)/[slug]/pdf/page.tsx
@@ -1,23 +1,20 @@
-// import { fetchPropertyByID } from '#/backend/apisConnections';
-// import { PropertyBroucher } from '@/components/PropertyPageBrochure';
-// import { PdfParent, CreatePdf } from '@/components/PdfPageView';
+import { fetchPropertyByID } from '#/backend/apisConnections';
+import { PropertyBroucher } from '@/components/PropertyPageBrochure';
+import { PdfParent, CreatePdf } from '@/components/PdfPageView';
 
-export default function hello() {
-    return <div>Hello</div>;
+type PageParams = { slug: string }
+export default async function PdfView({ params }: { params: Promise<PageParams>; }) {
+    const resolvedParams = await params; 
+    const fetchedProperty = await fetchPropertyByID(resolvedParams.slug);
+
+    if (!fetchedProperty) {
+        return <div>--page not found exec--</div>;
+    }
+
+    const pdf = new PdfParent(fetchedProperty);
+    const brochure = <PropertyBroucher property={fetchedProperty} flag={true} />;
+
+    return (
+        <CreatePdf pdf={pdf} brochure={brochure} />
+    );
 }
-
-// type PageParams = { slug: string }
-// export default async function PdfView({ params }: { params: PageParams; }) {
-//     const fetchedProperty = await fetchPropertyByID(params.slug);
-
-//     if (!fetchedProperty) {
-//         return <div>--page not found exec--</div>;
-//     }
-
-//     const pdf = new PdfParent(fetchedProperty);
-//     const brochure = <PropertyBroucher property={fetchedProperty} flag={true} />;
-
-//     return (
-//         <CreatePdf pdf={pdf} brochure={brochure} />
-//     );
-// }
