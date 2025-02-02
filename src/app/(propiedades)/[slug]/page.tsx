a7b779840c344df76e9c19a90686ee2c730983c2
vctrubio <vctrubio@gmail.com>
2025-02-03 00:35:35 +0100

    metadata init

--- src/app/(propiedades)/[slug]/page.tsx
+++ src/app/(propiedades)/[slug]/page.tsx
@@ -20,17 +20,18 @@ const fetchProperty = cache(async (slug: string) => {
 });
 
 async function getPropertyData(params: PageParams) {
-    return fetchProperty(params.slug);
+    const propertyData = await fetchProperty(params.slug);
+    return propertyData.property;
 }
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const resolvedParams = await params; // Await the params
-    const { property, slug } = await getPropertyData(resolvedParams);
+    const resolvedParams = await params;
+    const property = await getPropertyData(resolvedParams);
 
     if (!property) {
         return {
-            title: 'Property Not Found | LHS Concept',
-            description: 'The requested property could not be found'
+            title: 'LHS Concept',
+            description: 'Propiedades de lujo en Madrid.'
         };
     }
 
@@ -43,12 +44,12 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
         openGraph: {
             title: propertyTitle,
             description: propertyDescription,
-            url: `https://www.lhsconcept.com/propiedades/${slug}`,
-            images: property.photos_url ? [
+            url: `https://www.lhsconcept.com/${resolvedParams.slug}`,
+            images: property.photos_cover_url ? [
                 {
-                    url: property.photos_url[0].url,
-                    width: property.photos_url[0].width,
-                    height: property.photos_url[0].height,
+                    url: property.photos_cover_url[0].url,
+                    width: property.photos_cover_url[0].width,
+                    height: property.photos_cover_url[0].height,
                     alt: property.title,
                 }
             ] : [],
@@ -60,10 +61,10 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
             creator: '@lhsconcept',
             title: propertyTitle,
             description: propertyDescription,
-            images: property.photos_url ? [property.photos_url[0].url] : [],
+            images: property.photos_url ? [property.photos_cover_url[0].url] : [],
         },
         alternates: {
-            canonical: `https://www.lhsconcept.com/${slug}`,
+            canonical: `https://www.lhsconcept.com/${resolvedParams.slug}`,
         },
         robots: {
             index: true,
@@ -78,7 +79,7 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
 
 export default async function Page({ params }: Props) {
     const resolvedParams = await params;
-    const { property } = await getPropertyData(resolvedParams);
+    const property = await getPropertyData(resolvedParams);
 
     if (!property) {
         return <NotFound />;
--- src/app/(propiedades)/[slug]/pdf/page.tsx
+++ src/app/(propiedades)/[slug]/pdf/page.tsx
@@ -1,18 +1,89 @@
 import { fetchPropertyByID } from '#/backend/apisConnections';
 import { PropertyBroucher } from '@/components/PropertyPageBrochure';
 import { PdfParent, CreatePdf } from '@/components/PdfPageView';
+import { cache } from 'react';
+import { Metadata } from 'next';
+
+type Props = {
+    params: Promise<PageParams>;
+}
+
+const fetchProperty = cache(async (slug: string) => {
+    return {
+        slug,
+        property: await fetchPropertyByID(slug)
+    };
+});
+
+async function getPropertyData(params: PageParams) {
+    const propertyData = await fetchProperty(params.slug);
+    return propertyData.property;
+}
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+    const resolvedParams = await params;
+    const property = await getPropertyData(resolvedParams);
+
+    if (!property) {
+        return {
+            title: 'LHS Concept',
+            description: 'Propiedades de lujo en Madrid.'
+        };
+    }
+
+    const propertyTitle = `${property.title} | Ficha | LHS Concept`;
+    const propertyDescription = `€${property.precio.toLocaleString('es-ES')} 📍${property.barrioRef.name}`
+    
+    return {
+        title: propertyTitle,
+        description: propertyDescription,
+        openGraph: {
+            title: propertyTitle,
+            description: propertyDescription,
+            url: `https://www.lhsconcept.com/${resolvedParams.slug}/pdf`,
+            images: property.photos_cover_url ? [
+                {
+                    url: property.photos_cover_url[0].url,
+                    width: property.photos_cover_url[0].width,
+                    height: property.photos_cover_url[0].height,
+                    alt: property.title,
+                }
+            ] : [],
+            type: 'website',
+        },
+        twitter: {
+            card: 'summary_large_image',
+            site: '@lhsconcept',
+            creator: '@lhsconcept',
+            title: propertyTitle,
+            description: propertyDescription,
+            images: property.photos_cover_url ? [property.photos_cover_url[0].url] : [],
+        },
+        alternates: {
+            canonical: `https://www.lhsconcept.com/${resolvedParams.slug}/pdf`,
+        },
+        robots: {
+            index: true,
+            follow: true,
+            googleBot: {
+                index: true,
+                follow: true,
+            },
+        },
+    };
+}
 
 type PageParams = { slug: string }
 export default async function PdfView({ params }: { params: Promise<PageParams>; }) {
     const resolvedParams = await params;
-    const fetchedProperty = await fetchPropertyByID(resolvedParams.slug);
+    const property = await getPropertyData(resolvedParams);
 
-    if (!fetchedProperty) {
+    if (!property) {
         return <div>--page not found exec--</div>;
     }
 
-    const pdf = new PdfParent(fetchedProperty);
-    const brochure = <PropertyBroucher property={fetchedProperty} flag={true} />;
+    const pdf = new PdfParent(property);
+    const brochure = <PropertyBroucher property={property} flag={true} />;
 
     return (
         <CreatePdf pdf={pdf} brochure={brochure} />
