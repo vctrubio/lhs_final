eae707c363f63f2534d77ff324be8ecefad7ed0c
vctrubio <vctrubio@gmail.com>
2025-02-03 02:35:09 +0100

    bug out of my pants. hooray

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -104,8 +104,8 @@ export default async function RootLayout({
   return (
     <html lang="es" className={eczar.variable}>
       <head>
-        {/* <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" /> */}
-        <meta name="viewport" content="width=device-width" />
+        {/* <meta name="viewport" content="width=device-width" /> */}
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
