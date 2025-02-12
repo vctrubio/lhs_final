ff5f93844ac6780162d8988985f402193e0ef893
vctrubio <vctrubio@gmail.com>
2025-02-12 17:50:32 +0100

    thin font for logo

--- src/components/lhsbond.jsx
+++ src/components/lhsbond.jsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 export function LHSBond({ title = 'lhs', concept = 'Concept' }) {
     return (
         <div className='flex-col [&_*]:flex font-ariel py-1 shadow-inner shadow-sm'>
-            <div className='h-4/5 justify-center h-[62px] text-[26px] font-semibold'>
+            <div className='h-4/5 justify-center h-[62px] text-[28px]'>
                     <Link href='/'>
                         <div className='justify-center items-center mx-auto 
                         text-center tracking-wide uppercase w-[62px] h-[62px]
