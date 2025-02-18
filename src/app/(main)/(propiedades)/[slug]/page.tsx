193f32fbf9138df2e932f4e67cad993c30d9625c
vctrubio <vctrubio@gmail.com>
2025-02-18 16:15:01 +0100

    page props params fix

--- src/app/(main)/(propiedades)/[slug]/page.tsx
+++ src/app/(main)/(propiedades)/[slug]/page.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import type { Metadata } from 'next';
 import PropertyPage from '@/components/PropertyPage';
 import NotFound from '@/app/(main)/not-found';
 import { getPropertyData, generatePropertyMetadata, Props } from '@/utils/metadata';
 
 export async function generateMetadata({ params }: Props): Promise<Metadata> {
-    const resolvedParams = await params; // Ensure params is awaited if it's a promise
+    const resolvedParams = await params;
     const property = await getPropertyData(resolvedParams);
-    return generatePropertyMetadata(property, resolvedParams.slug); // Use resolvedParams.slug directly
+    return generatePropertyMetadata(property, resolvedParams.slug);
 }
 
 export default async function Page({ params }: Props) {
-    const resolvedParams = await params; // Ensure params is awaited if it's a promise
+    const resolvedParams = await params;
     const property = await getPropertyData(resolvedParams);
 
     if (!property) {
@@ -20,6 +19,5 @@ export default async function Page({ params }: Props) {
 
     return (
         <PropertyPage property={property} />
-    );
+    )
 }
-
--- src/utils/metadata.ts
+++ src/utils/metadata.ts
@@ -2,10 +2,8 @@ import type { Metadata } from 'next';
 import { fetchPropertyByID } from '#/backend/apisConnections';
 import { cache } from 'react';
 
-export type PageParams = { slug: string };
-
 export type Props = {
-    params: Promise<PageParams>;
+    params: Promise<{ slug: string }>;
 };
 
 export const fetchProperty = cache(async (slug: string) => {
@@ -15,9 +13,8 @@ export const fetchProperty = cache(async (slug: string) => {
     };
 });
 
-export async function getPropertyData(paramsPromise: Promise<{ slug: string }>) {
-    const params = await paramsPromise;
-    const propertyData = await fetchProperty(params.slug);
+export async function getPropertyData({ slug }: { slug: string }) {
+    const propertyData = await fetchProperty(slug);
     return propertyData.property;
 }
 
@@ -28,7 +25,7 @@ export function generatePropertyMetadata(property: any, slug: string, pdf: boole
             description: 'Encontramos propiedades exclusivas en Madrid.',
             openGraph: {
                 title: 'LHS Concept',
-                description: 'Con ms de 20 años de experiencia en el mercado inmobiliario de lujo, nuestra reputación está garantizada por la satisfacción de nuestros clientes.\nNos especializamos en ayudar a nuestros clientes comprar y vender propiedades de lujo en las ubicaciones más exclusivas de Madrid. Con una experiencia inigualable en el mercado y una pasión por ofrecer un servicio excepcional, estamos dedicados a hacer que su experiencia inmobiliaria sea fluida y gratificante.',
+                description: 'Con más de 20 años de experiencia en el mercado inmobiliario de lujo, nuestra reputación está garantizada por la satisfacción de nuestros clientes.\nNos especializamos en ayudar a nuestros clientes a comprar y vender propiedades de lujo en las ubicaciones más exclusivas de Madrid. Con una experiencia inigualable en el mercado y una pasión por ofrecer un servicio excepcional, estamos dedicados a hacer que su experiencia inmobiliaria sea fluida y gratificante.',
                 url: `https://www.lhsconcept.com/`,
                 images: [
                     {
