9c27dd668ab9855afe448bf21b6367fb18edcf89
vctrubio <vctrubio@gmail.com>
2025-02-21 15:08:55 +0100

    user-scalability

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,23 @@
 import "../css/globals.css";
 import type { Metadata, Viewport } from "next";
 import React from "react";
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react";
 
 export const viewport: Viewport = {
-    width: 'device-width',
+    width: "device-width",
     initialScale: 1,
     maximumScale: 1,
-    userScalable: false,
-    viewportFit: 'cover',
+    userScalable: true,
+    viewportFit: "cover",
     interactiveWidget: "resizes-content", // Fixes iOS keyboard pushing content
-}
+};
 
 export const metadata: Metadata = {
     title: "LHS Concept | Propiedades Selectas en Madrid",
-    description: "Expertos en propiedades de lujo en Madrid. Compra y vende inmuebles exclusivos con asesoramiento personalizado.",
-    keywords: "Inmuebles de lujo, Inmobiliaria Madrid, Propiedades exclusivas, Venta de apartamentos, Alquiler de pisos Madrid, LHS Concept, LHS Madrid",
+    description:
+        "Expertos en propiedades de lujo en Madrid. Compra y vende inmuebles exclusivos con asesoramiento personalizado.",
+    keywords:
+        "Inmuebles de lujo, Inmobiliaria Madrid, Propiedades exclusivas, Venta de apartamentos, Alquiler de pisos Madrid, LHS Concept, LHS Madrid",
     icons: {
         icon: "/icon.png",
         shortcut: "/icon.png",
@@ -23,7 +25,8 @@ export const metadata: Metadata = {
     },
     openGraph: {
         title: "LHS Concept | Propiedades de Lujo en Madrid",
-        description: "Compra y vende inmuebles exclusivos en Madrid con asesoramiento premium.",
+        description:
+            "Compra y vende inmuebles exclusivos en Madrid con asesoramiento premium.",
         type: "website",
         url: "https://www.lhsconcept.com",
         images: [
@@ -43,7 +46,8 @@ export const metadata: Metadata = {
         card: "summary_large_image",
         site: "@lhsconcept",
         title: "LHS Concept | Propiedades Exclusivas en Madrid",
-        description: "Compra y vende inmuebles de lujo en Madrid con expertos en propiedades exclusivas.",
+        description:
+            "Compra y vende inmuebles de lujo en Madrid con expertos en propiedades exclusivas.",
         images: ["https://www.lhsconcept.com/logo-main.jpeg"],
     },
     robots: { index: true, follow: true },
@@ -95,7 +99,6 @@ export default async function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-
     return (
         <html lang="es">
             <head>
@@ -109,4 +112,3 @@ export default async function RootLayout({
         </html>
     );
 }
-
